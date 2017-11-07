210eaf53b25b78b263e3a9212a85d7e6f3c620c6
Tommy Svensson <tommy@logicinteractive.no>
2017-11-07 18:08:12 +0100

    Small cleanup

--- $(BaseDir)/Projects/700 Unreal.hx - UE4.17 Project/tools/utils/CopyCppFiles.bat
+++ $(BaseDir)/Projects/700 Unreal.hx - UE4.17 Project/tools/utils/CopyCppFiles.bat
@@ -1,2 +1,2 @@
 for /f "delims=" %%F in ('dir %~dp0\..\..\*.uproject /b /o-n') do set file=%%~nF
-xcopy "c:/dev/UnrealHX/Lib/shared/cpp" "../Source/%file%" /h/i/c/k/e/r/y
+xcopy "...your source dir..." "../Source/%file%" /h/i/c/k/e/r/y
--- $(BaseDir)/Projects/700 Unreal.hx - UE4.17 Project/tools/utils/CopyExterns.bat
+++ $(BaseDir)/Projects/700 Unreal.hx - UE4.17 Project/tools/utils/CopyExterns.bat
@@ -1,1 +1,1 @@
-xcopy "c:/dev/UnrealHX/Setup/Externs" "../../Plugins/UnrealHx/Haxe/Externs/Common/unreal" /h/i/c/k/e/r/y
+xcopy "...your source dir" "../../Plugins/UnrealHx/Haxe/Externs/Common/unreal" /h/i/c/k/e/r/y
